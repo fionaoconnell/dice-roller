818e23569803298076d5e5172729c5ac875a1c62
Fiona O'Connell <fionaaileen13@gmail.com>
2018-12-23 02:30:19 -0600

    rolling seeems to be working, check neg mods

--- Diceroller.hs
+++ Diceroller.hs
@@ -43,16 +43,8 @@ rollNTimes n sides gen = do
    let (ndice, stdgen) = runState (replicateM n (rolladie sides)) gen
    pure $ foldr (+) 0 ndice
 
-
---(*d*) :: Int -> Int -> RandState Int
---------------------------------------------------------------------------------
-
 --------------------------------------------------------------------------------
 -- The parser.
-
-type Quantity = Int
-type Sides = Int
-type Number = Int
 
 data RollExp
     = Roll_d_ Int Int
@@ -85,21 +77,28 @@ parseRoll_d_ = do
 parseRoll :: ReadP RollExp
 parseRoll = chainl1 (parseRoll_d_ <++ (Modifier <$> parseInt)) (parsePlus +++ parseMinus)
 
--- Running the parser-----------------------------------------------------------
+evalRollExp :: Maybe RollExp -> Maybe (IO Int)
+evalRollExp exp =
+    case exp of
+        Just (Roll_d_ n die) -> Just $ newStdGen >>= (\gen -> return (runRandom (rollNTimes n die gen) gen))
+        Just (Modifier n) -> Just $ return n
+        Just (Plus  e1 e2) -> liftA2 (liftA2 (+)) (evalRollExp (Just e1)) (evalRollExp (Just e2))
+        Just (Minus e1 e2) -> liftA2 (liftA2 subtract) (evalRollExp (Just e1)) (evalRollExp (Just e2))
+        Nothing -> Nothing
+
 -- Parse function from Lab6, rewrite it later.
 parse :: String -> Maybe RollExp
 parse str =
     case (completeParses, incompleteParses) of
         ([(result, "")], _  ) -> Just result  -- Only complete result.
-        ([]            , [] ) -> Nothing--Left $ "No parse."
-        ([]            , _:_) -> Nothing--Left $ "Incomplete parse. Unparsed: " ++ show leastRemaining
-        (_:_           , _  ) -> Nothing--Left $ "Ambiguous parse: " ++ show completeParses
+        ([]            , [] ) -> Nothing
+        ([]            , _:_) -> Nothing
+        (_:_           , _  ) -> Nothing
     where
         parses = readP_to_S parseRoll str
         (completeParses, incompleteParses) =
             partition (\(_, remaining) -> remaining == "") parses
         leastRemaining = minimumBy (comparing length) . map snd $ incompleteParses
-
 
 main :: IO ()
 main = do
@@ -110,68 +109,3 @@ main = do
      realout <- out
      putStrLn $ realout
      main
-
---  runInputT defaultSettings superRepl
-
-
-evalRollExp :: Maybe RollExp -> Maybe (IO Int)
-evalRollExp exp =
-    case exp of
-        Just (Roll_d_ n die) -> Just $ newStdGen >>= (\gen -> return (runRandom (rollNTimes n die gen) gen))
-        Just (Modifier n) -> Just $ return n
-        Just (Plus  e1 e2) -> liftA2 (liftA2 (+)) (evalRollExp (Just e1)) (evalRollExp (Just e2))
-        Just (Minus e1 e2) -> liftA2 (liftA2 subtract) (evalRollExp (Just e1)) (evalRollExp (Just e2))
-        Nothing -> Nothing
---
--- -- Use if Haskeline doesn't work.
--- simpleRepl :: IO ()
--- simpleRepl = do
---     putStr "> "
---     hFlush stdout
---     line <- getLine
---     case handleReplLine line of
---         Nothing        -> return ()    -- Time to quit.
---         Just resultStr -> do
---             resultStr
---             simpleRepl
---
--- handleReplLine :: String -> Maybe (IO String)
--- handleReplLine line =
---     if line `elem` ["q", ":q", "quit", ":quit", "exit"] then
---         Nothing
---     else
---         let
---             unparsedStr =
---                 case parse line of
---                     Right parsed -> unparseRollExp parsed ++ "\n"
---                     _            -> ""
---         in
---         Just $
---             liftA2 (++) (pure  ((show . parse $ line) ++ "\n" ++ unparsedStr)) (showParseEval line)
---
---
--- unparseRollExp :: RollExp -> String
--- unparseRollExp (Roll_d_ n die)     = show n ++ "d" ++ show die
--- unparseRollExp (Modifier n) = show n
--- -- unparseRollExp (Neg    e1)    = "-" ++ unparseRollExp e1
--- unparseRollExp (Plus   e1 e2) = "(" ++ unparseRollExp e1 ++ " + " ++ unparseRollExp e2 ++ ")"
--- unparseRollExp (Minus  e1 e2) = "(" ++ unparseRollExp e1 ++ " - " ++ unparseRollExp e2 ++ ")"
---
--- -- If second argument is Left, prefix the error message
--- -- with the first argument. Otherwise, pass-through.
--- prefixError :: String -> Either String a -> Either String a
--- prefixError prefix (Left errMsg) = Left (prefix ++ errMsg)
--- prefixError _      rightResult   = rightResult
---
---
--- tryParseEval :: String -> Either String (IO Int)
--- tryParseEval expStr =
---     parse expStr >>= (prefixError "Evaluation error: " . eval)
---
---
--- -- Convert result to string or return error message.
--- showParseEval :: String -> IO String
--- showParseEval expStr =
---     case tryParseEval expStr of
---       Right result  -> show <$> result
---       Left errorMsg -> pure errorMsg
